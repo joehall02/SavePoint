fa94d2a39cc17afd87c4c0c7ad947a28e23d3586
joehall02 <93493198+joehall02@users.noreply.github.com>
2025-12-20 14:53:30 +0000

    - Updated gameSchema to use enum of condition strings when validating condition

--- src/schemas/gameSchema.ts
+++ src/schemas/gameSchema.ts
@@ -1,14 +1,22 @@
 import * as z from "zod";
 
+const CONDITIONS = [
+  "Very Good",
+  "Good",
+  "Okay",
+  "Bad",
+  "Very Bad"
+] as const
+
 export const createGameSchema = z.object({
   title: z.string(),
-  condition: z.string(),
+  condition: z.enum(CONDITIONS),
   notes: z.string(),
   boxIncluded: z.boolean(),
   rating: z.number(),
   igdbId: z.number(),
   platformId: z.number(),
-});
+})
 
 export const getGameDetailsSchema = z.object({
   title: z.string(),
@@ -28,18 +36,18 @@ export const getAllGamesSchema = z.object({
 export const editGameSchema = z
   .object({
     title: z.string().optional(),
-    condition: z.string().optional(),
+    condition: z.enum(CONDITIONS).optional(),
     notes: z.string().optional(),
     boxIncluded: z.boolean().optional(),
     rating: z.number().optional(),
     platformId: z.number().optional(),
   })
-  .strict() // Throw error if any unknown attributes are provided
+  .strict() // Throw error if any unknown attributes are provided, even if known values are passed
   .refine((data) => {
-    // Itterate over object to determine if one of the attributes is present
+    // Itterate over object to determine if one of the attributes is present, if not then throw
     // Data: ZodObject
     // .some: returns true if callback functions returns true for an elements in array
-    // (value) => value !== undefined: callback function, determine if the value
+    // (value) => value !== undefined: callback function, determine if the value is not undefined
     return Object.values(data).some((value) => value !== undefined);
 });
 
